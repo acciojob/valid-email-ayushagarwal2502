0a54d85e45216694a9bb5f14173fbf431f6c145f
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-10 02:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function validEmail(str) {
   //your JS code here.
+	let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	if(str.includes()==emailRegex){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
-
+ let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if(emailRegex.test(str)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
